5c538bf0ddeafd858deabfe7376cf1defcc82016
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-03-14 23:38:52 +0300

    init

---
+++ project/tests/src/test.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+#include <iostream>
+
+extern "C" {
+    #include "matrix.h"
+}
+
+TEST(first_test, malloc_matrix) {
+    Matrix* t = malloc_matrix(1, 1);
+    ASSERT_EQ(t->rows, 1);
+}
+
+TEST(create_matrix, case_1) {
+
+    FILE* l = fopen("project/tests/data/case_1/left", "r");
+    FILE* r = fopen("project/tests/data/case_1/right", "r");
+
+    Matrix* left = create_matrix(l);
+    Matrix* right = create_matrix(r);
+
+    ASSERT_TRUE(is_matrix_equal(left, right));
+
+    free_matrix(left);
+    free_matrix(right);
+
+    fclose(l);
+    fclose(r);
+}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
