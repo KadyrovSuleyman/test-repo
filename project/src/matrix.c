fbaeb106a9385f1e7e703a3ffeb75676f015a783
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-03-15 18:30:16 +0300

    sdfsdf

--- project/src/matrix.c
+++ project/src/matrix.c
@@ -56,22 +56,22 @@ Matrix* malloc_matrix(const size_t rows, const size_t cols) {
     result->cols = cols;
 
     result->item = (double**)malloc(rows * sizeof(double*));
-    if (unlikely(error_handler("malloc"))) {
-        free(result->item);
-        free_matrix(result);
-        return NULL;
-    }
+    // if (unlikely(error_handler("malloc"))) {
+    //     free(result->item);
+    //     free_matrix(result);
+    //     return NULL;
+    // }
 
     for (size_t i = 0; i < rows; ++i) {
         result->item[i] = (double*)malloc(cols * sizeof(double));
-        if (unlikely(error_handler("malloc"))) {
-            for (size_t j = 0; j < i; ++j) {
-                free(result->item[j]);
-            }
-            free(result->item);
-            free_matrix(result);
-            return NULL;
-        }
+        // if (unlikely(error_handler("malloc"))) {
+        //     for (size_t j = 0; j < i; ++j) {
+        //         free(result->item[j]);
+        //     }
+        //     free(result->item);
+        //     free_matrix(result);
+        //     return NULL;
+        // }
     }
 
     return result;
